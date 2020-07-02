8acf8a5e5fa09c0221addcd1097e9573494dfda2
Roberto Zaniboni <zaniboni@gmail.com>
2020-07-02 12:12:46 +0200

    Add Search and Detail Restautants

--- OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -1,25 +1,30 @@
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
 using OdeToFood.Core;
 using OdeToFood.Data;
 
-namespace OdeToFood.Pages.Restaurants
-{
+namespace OdeToFood.Pages.Restaurants {
   public class ListModel : PageModel {
     private readonly IConfiguration config;
-    private readonly IRestaurantData restaurantData;
 
+    private readonly IRestaurantData restaurantData;
     public string Message { get; set; }
+
+    [BindProperty(SupportsGet = true)]
+    public string SearchTerm { get; set; }
+
     public IEnumerable<Restaurant> Restaurants { get; set; }
     public ListModel (IConfiguration config, IRestaurantData restaurantData) {
       this.config = config;
       this.restaurantData = restaurantData;
     }
 
-    public void OnGet () {
+    public void OnGet (string searchTerm) {
+
       Message = config["Message"];
-      Restaurants = restaurantData.GetAll ();
+      Restaurants = restaurantData.GetRestaurantsByName (SearchTerm);
     }
   }
 }
