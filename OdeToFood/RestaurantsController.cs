bc26ff188f15d00e03de94e825d333721c3780a1
Roberto Zaniboni <zaniboni@gmail.com>
2020-07-03 11:06:56 +0200

    Add apicontroller and datatable sample component

---
+++ OdeToFood/RestaurantsController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OdeToFood
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RestaurantsController : ControllerBase
+    {
+    }
+}
--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -27,8 +27,9 @@ namespace OdeToFood {
       });
 
       services.AddScoped<IRestaurantData, SqlRestaurantData> ();
-      
+
       services.AddRazorPages ();
+      services.AddControllers ();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +51,7 @@ namespace OdeToFood {
 
       app.UseEndpoints (endpoints => {
         endpoints.MapRazorPages ();
+        endpoints.MapControllers ();
       });
     }
   }
